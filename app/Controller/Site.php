36101cda0475898d738a4e288e5a74516d5e46e8
savin ilya <savin15ilya3112@gmail.com>
2026-04-24 01:57:40 +0700

    data validation. security audit (methodological guide)

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,24 +2,47 @@
 
 namespace Controller;
 
+use Model\User;
+use Src\Request;
+use Src\View;
+use Src\Validator\Validator;
+
 class Site
 {
-//    public function index(Request $request): string
-//    {
-//        $posts = Post::where('id', $request->id)->get();
-//        return (new View())->render('site.post', ['posts' => $posts]);
-//    }
+    public function index(Request $request): string
+    {
+        $posts = Post::where('id', $request->id)->get();
+        return (new View())->render('site.post', ['posts' => $posts]);
+    }
 
-//    public function hello(): string
-//    {
-//        return (new View())->render('site.hello', ['message' => 'hello working']);
-//    }
-//
-//    public function signup(Request $request): string
-//    {
-//        if ($request->method === 'POST' && User::create($request->all())) {
-//            app()->route->redirect('/go');
-//        }
-//        return new View('site.signup');
-//    }
+    public function hello(): string
+    {
+        return (new View())->render('site.hello', ['message' => 'hello working']);
+    }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method === 'POST') {
+
+            $validator = new Validator($request->all(), [
+                'name' => ['required'],
+                'login' => ['required', 'unique:users,login'],
+                'password' => ['required']
+            ], [
+                'required' => 'Поле :field пусто',
+                'unique' => 'Поле :field должно быть уникально'
+            ]);
+
+            if($validator->fails()){
+                return new View('site.signup',
+                    ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+            }
+
+            if (User::create($request->all())) {
+                app()->route->redirect('/login');
+            }
+        }
+        return new View('site.signup');
+    }
+
 }
