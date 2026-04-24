df0ed7db154f15f3171e46ea6f4cb15b25118677
savin ilya <savin15ilya3112@gmail.com>
2026-04-25 06:20:08 +0700

    added unittest SiteTest

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -45,4 +45,32 @@ class Site
 //        return new View('site.signup');
 //    }
 
+//    public function signup(Request $request): string
+//    {
+//        if ($request->method === 'POST') {
+//
+//            $validator = new Validator($request->all(), [
+//                'name' => ['required'],
+//                'login' => ['required', 'unique:users,login'],
+//                'password' => ['required']
+//            ], [
+//                'required' => 'Поле :field пусто',
+//                'unique' => 'Поле :field должно быть уникально'
+//            ]);
+//
+//            if($validator->fails()){
+//                return new View('site.signup',
+//                    ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+//            }
+//
+//            if (User::create($request->all())) {
+//                app()->route->redirect('/login');
+//            }
+//        }
+//        if (User::create($request->all())) {
+//            app()->route->redirect('/login');
+//            return false;
+//        }
+//        return new View('site.signup');
+//    }
 }
